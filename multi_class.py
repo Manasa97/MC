be8c0f62fd8063bdd95d9a404b8271e1e45b0021
Manasa97 <manasa.nallabotula@gmail.com>
2018-02-18 12:05:00 +0530

    Update multi_class.py

--- multi_class.py
+++ multi_class.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import pandas as pd
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -8,19 +9,11 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 
-train_data = np.array(list(csv.reader(open("train.csv"))))
-test_data = np.array(list(csv.reader(open("qaps_2000_cleaned.csv"))))
+train = pd.read_csv("train.csv")
+test = pd.read_csv("qaps_2000_cleaned.csv")
 
-coarse_train_target = train_data[:,0]
-fine_train_target = train_data[:,1]
-train_text = train_data[:,2]
-coarse_test_target = test_data[:,1]
-fine_test_target = test_data[:,2]
-test_text = test_data[:,0]
-
-train_target = fine_train_target
-test_target = fine_test_target
-
+train.columns = ["coarse", "fine", "text"]
+test.columns = ["text", "coarse", "fine"]
 
 pipeline = Pipeline([
     ('count_vectorizer',   CountVectorizer()),
@@ -30,14 +23,14 @@ pipeline = Pipeline([
 
 
 lb = LabelBinarizer()
-train_z = lb.fit_transform(train_target)
+z = lb.fit_transform(train["coarse"])
 
-pipeline.fit(train_text, train_z)
-predicted = pipeline.predict(test_text)
+pipeline.fit(train["text"], z)
+predicted = pipeline.predict(test["text"])
 predictions = lb.inverse_transform(predicted)
 print(len(predictions))
 
 
-accuracy = accuracy_score(test_target, predictions)
+accuracy = accuracy_score(test["coarse"], predictions)
 print(accuracy)
 
